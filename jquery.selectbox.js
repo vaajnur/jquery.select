07837bf9be018a7fb605f974fdc452a2230e1bff
vaajnur <ainur.valiev@mail.ru>
2017-03-03 17:08:11 +0300

    комменты

--- jquery.selectbox.js
+++ jquery.selectbox.js
@@ -1,9 +1,11 @@
 ;(function($){
+	// стандартный способ расширить апи jQuery это добавление к прототипу объекта свой метод
 $.fn.customSelect = function (){
 	$(this).each( function(){
+		// прячем ориг. селект
 		$(this).addClass("hidden")
 
-		// vars
+		// создаем dom элемент-эмуляцию селекта, определяем переменные
 		var select_cont = $("<div class='select_cont'>"),
 			div_select = $("<div class='div_select'>"),
 			num = $(this).find("option").length,
@@ -11,7 +13,7 @@ $.fn.customSelect = function (){
 			that = $(this),
 			opened = false
 
-		// create select dropdown emulate
+		// создаем событие открытия селекта
 		div_select[0].addEventListener("click", function(){
 			if(opened == false){
 				$(this).height((num+1)*28 + "")
@@ -25,48 +27,52 @@ $.fn.customSelect = function (){
 
 		})
 
+		// если есть в селекте оптионы
 		if(num > 0){
-			var i = 0,
+			var i = 0, // счетчик
 				selected = null
 			while(i < num){
-				// create option emulate
+				// создаем dom элемент эмуляцию option
 				var div_option = 
 					$("<div class='div_option "+ ((options.eq(i).is(':selected')) ? ' selected' : '' ) +"' data-value='"+ options.eq(i).val() + "'>").html(options.eq(i).text())
 					
-				// save selected index
-				options.eq(i).is(':selected') ? selected = i : null 
-				// add events to emuled options
+				// если есть ориг. оптион с атрибутом selected, эмуляция оптиона будет в начале
+				options.eq(i).is(':selected') ? selected = true : null 
+				// добавляем обработчики для нажатия на оптион
 				div_option[0].addEventListener("click", function(){
-					// clear selected attribute
+					// очищаем выбранный элемент в ориг. селекте
 					that.find("option").removeAttr("selected")
 					;(function(x){
 						var i2 = 0
 						var ind = $(x).data("value")
-						/*  add selected attr to true option  */
+						/*  делаем выбранным оптоин в ориг. селекте  */
 						while(i2 < num){
 							if(options.eq(i2).val() == ind){
 								that.find("option").eq(i2).attr("selected", "selected")
 							}
 							i2++;
 						}
-						// move emuled option to begin, like option in select
+						// перемещаем эмуляцию оптоиона наверх как в обычном селекте
 						$(x).parent().find(".div_option").eq(0).remove()
+						// клон выбранного оптоина наверх
 						$(x).clone(true).prependTo(div_select)
+						// очищаем выбранный элменет в эмулир. селекте
 						$(".div_option").removeClass("selected")
+						// 
 						$(x).addClass("selected")
 					})(this)
 				})
-				// Add 1st default option, empty by default or selected if isset
+				// Первый элемент в эмулир. селекте будет 1-й оптион или выбранный оптион с параметром selected
 				if(i == 0 && selected == null){
 					div_select.prepend(div_option.clone(true)) 
 				}else if(selected != null){
-					// Remove previously added def option  
+					// Удаляем оптион по умолчанию  
 					div_select.find(".div_option").eq(0).remove()
-					// Add default selected option to begin
+					// Перемещаем клон эмуляции оптиона наверх
 					div_select.prepend(div_option.clone(true).removeClass("selected"))
 					selected = null 
 				}
-				// add created emule option in emuled select
+				// добавляем эмуляцию оптиона к эмул.селекта
 				div_select.append(div_option) 
 
 				i++;
@@ -75,7 +81,9 @@ $.fn.customSelect = function (){
 
 		}// endif
 
+		// добавляем эмул. селекта в отд. контейнер
 		select_cont.append(div_select)
+		// кастомиз. селект вставляем перед скрытым ориг. селектом
 		select_cont.insertBefore($(this))
 	})
 }
